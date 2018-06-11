90b834eddfbe2d291f2f8c57e085a7f1c0025fbe
yanming <1242162176@qq.com>
2018-06-11 22:37:54 +0800

    2

--- 33-project/src/main.js
+++ 33-project/src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import Store from './vuex/store'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
@@ -10,6 +15,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  Store,
   components: { App },
   template: '<App/>'
 })
